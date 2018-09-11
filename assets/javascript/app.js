dd683fc2b4666d3f639650845b0abe98fa775019
CircuitSyn <circuitsyn@gmail.com>
2018-09-11 01:35:07 -0700

    added clearing code as well as question data append

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,14 +12,15 @@ var gameVariables = {
 let gamePlayArray = [
     sonic = {
         question: 'Name a famous SEGA character that is ultra fast, was a flagship for the system and has over 100 titles to date!',
-        choice1: 'Purple Durple',
-        choice2: 'Buzby',
-        choice3: 'Sonic',
-        choice4: 'Tails',
+        choice1: '<button type="button" class="btn btn-outline-dark">A. Purple Durple</button>',
+        choice2: '<button type="button" class="btn btn-outline-dark">B. Buzby</button>',
+        choice3: '<button type="button" class="btn btn-outline-dark">C. Sonic</button>',
+        choice4: '<button type="button" class="btn btn-outline-dark">D. Tails</button>',
         rightAnswer: 'You got it right! Well done. The answer is Sonic!',
         wrongAnswer: 'You got it wrong! The correct answer is Sonic! You gott abe faster next time..!',
         gif: '<img class="images" src="../../assets/images/Sonic.gif" alt="Sonic The Hedgehog">',
         audio: '',
+        clicked: false,
     },
 
     mario = {
@@ -32,6 +33,7 @@ let gamePlayArray = [
         wrongAnswer: 'You got it wrong! The correct answer is Mario! You gott abe faster next time..!',
         gif: '<img class="images" src="../../assets/images/Mario.gif" alt="Mario">',
         audio: '',
+        clicked: false,
     }
 
 ];
@@ -46,7 +48,22 @@ console.log ('------------------');
 $('#startbtn').click(function(){
     $('#startbtn').css({"display":"none"});
     for (i = 0; i < gamePlayArray.length; i++) {
-        
+        //Clearing out Divs as precaution each time
+        $('#question').empty();
+        $('#choiceA').empty();
+        $('#choiceB').empty();
+        $('#choiceC').empty();
+        $('#choiceD').empty();
+        $('#time').empty();
+
+        //push attribute section
+
+        //push question details
+        $('#question').append(gamePlayArray[0].question);
+        $('#choiceA').append(gamePlayArray[0].choice1);
+        $('#choiceB').append(gamePlayArray[0].choice2);
+        $('#choiceC').append(gamePlayArray[0].choice3);
+        $('#choiceD').append(gamePlayArray[0].choice4);
     }
 });
 
